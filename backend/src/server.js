fb3e84328b979071c089a817ad9fbb9f2c47a338
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-10-15 15:29:36 +0800

    connected the database

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import TaskRoutes from './routes/TaskRoutes.js';
+import dotenv from 'dotenv';
+import connectDB from '../config/db.js';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5001;
+
+connectDB();
 
 app.use('/api/tasks', TaskRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
