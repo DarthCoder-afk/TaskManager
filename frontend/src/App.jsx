6948c5c434e3bcfa542f00e713e5c38f0104c289
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-10-19 16:35:53 +0800

    created two more pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import TaskDetailPage from './pages/TaskDetailPage.jsx'
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<HomePage />}/>
+          <Route path="/create" element={<CreatePage />} />
+          <Route path="/:id" element={<TaskDetailPage/>}/>
         </Routes>
       </div>
       
