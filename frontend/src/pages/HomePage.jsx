c2d140eb73934640a96f35e288fe5dbce723b6e3
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-10-21 13:42:06 +0800

    added a limiter ui

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 
 import React from 'react'
 import NavBar from '../components/NavBar'
+import Limiter from '../components/Limiter';
+import { useState , useEffect} from 'react';
 
 const HomePage = () => {
+  const [isLimiter, setLimiter] = useState(false);
+
   return (
-    <div>
+    <div className='min-h-screen'>
       <NavBar/>
+
+      {isLimiter && Limiter}
       
     </div>
   )
